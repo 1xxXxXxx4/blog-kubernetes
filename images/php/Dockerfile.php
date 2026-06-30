23a22b2b6a4816428790050f92daabe9d4307c3e
1xxXxXxx4 <144569341+1xxXxXxx4@users.noreply.github.com>
2026-06-30 11:33:03 +0000

    installation pdo_pgsql into php:fpm-alpine

--- images/php/Dockerfile.php
+++ images/php/Dockerfile.php
@@ -1,4 +1,6 @@
 FROM php:8.2-fpm-alpine
+RUN apk add --no-cache postgresql-dev
+RUN docker-php-ext-install pdo pdo_pgsql
 WORKDIR /usr/share/nginx/html
 COPY images/nginx/http/index.php . 
 EXPOSE 9000
